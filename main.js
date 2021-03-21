c52d8fc6f6467de551d9f0e3c309c6eadbf1324f
vladNew91 <vladnew91@gmail.com>
2021-03-21 19:43:40 +0300

    init: project

---
+++ main.js
@@ -0,0 +1,10 @@
+//fullscreen
+const btnFullscreen = document.querySelector('.fullscreen');
+
+btnFullscreen.addEventListener("click", () => {
+    if (!document.fullscreenElement) {
+        document.documentElement.requestFullscreen();
+    } else if (document.exitFullscreen) {
+        document.exitFullscreen();
+    }
+});
