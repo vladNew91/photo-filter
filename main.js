e6433f52f821adeee875ff56f8280cec4996c310
vladNew91 <vladnew91@gmail.com>
2021-04-26 02:22:27 +0300

    feat: button save with canvas

--- main.js
+++ main.js
@@ -48,7 +48,7 @@ inputs.forEach((input,i) => {
 
 //button reset
 const btnReset = document.querySelector(".btn-reset");
-
+//add to "button reset" listener "click"
 btnReset.addEventListener("click", () => {
     outputs.forEach((output,i) => {
         //set to all outputs and inputs dafault value
@@ -84,38 +84,43 @@ const images = [
     '20.jpg'
 ];
 let i = 0;
+let path = "https://raw.githubusercontent.com/rolling-scopes-school/" + 
+    "stage1-tasks/assets/images/";
+//get current hour
+const now = new Date().getHours();
+//set times 0f day
+let timesOfDay = 
+    now >= 0 && now <= 5 ? "night" : 
+    now >= 6 && now <= 11 ? "morning" : 
+    now >= 12 && now <= 17 ? "day" : 
+    now >= 18 && now <= 23 ? "evening" : false;
+
+const imgAdress = () => {
+    //set adress to image
+    img.src = path + `${timesOfDay}` + "/" + `${images[i]}`;
+    i++;
+    //disable button "next picture" for 1s
+    btnNext.disabled = true;
+    setTimeout(() => btnNext.disabled = false , 1000);
+};
 //add to button "next picture" listener "click"
 btnNext.addEventListener("click", () => {
-    console.log("imege " + (i+1));
-    //get current hour
-    const now = new Date().getHours();
-    //get times 0f day
-    let timesOfDay = 
-        now >= 0 && now <= 5 ? "night" : 
-        now >= 6 && now <= 11 ? "morning" : 
-        now >= 12 && now <= 17 ? "day" : 
-        now >= 18 && now <= 23 ? "evening" : false;
     //loop pictures by circle
-    if (i < images.length) {
-        //set adress to image
-        img.src = 
-            "https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks" +
-            "/assets/images/" + `${timesOfDay}` + "/" + `${images[i]}`;
-        i++;
-        //disable button "next picture" for 1s
-        btnNext.disabled = true;
-        setTimeout(() => btnNext.disabled = false , 1000);
-    } else i = 0;
+    if (i < images.length) imgAdress();
+    else {
+        i = 0;
+        imgAdress();
+    };
 });
 
 //load picture
 const btnLoad = document.querySelector(".btn-load--input");
-const imageContainer = document.querySelector(".img-container");
 //add to button "load picture" listener "change"
 btnLoad.addEventListener('change', (e) => {
-  //choose file
+  //choose one file
   const file = btnLoad.files[0];
   const reader = new FileReader();
+  //load file
   reader.onload = () => {
     img.src = reader.result;
   }
@@ -125,3 +130,45 @@ btnLoad.addEventListener('change', (e) => {
 
 //button "save picture"
 const btnSave = document.querySelector(".btn-save");
+const canvas = document.querySelector('canvas');
+
+btnSave.addEventListener("click", function(e) {
+    //get value of current filters on image
+    const blur = document.querySelector("input[name=blur]").value;
+    const invert = document.querySelector("input[name=invert]").value;
+    const sepia = document.querySelector("input[name=sepia]").value;
+    const saturate = document.querySelector("input[name=saturate]").value;
+    const hue = document.querySelector("input[name=hue]").value;
+    //create new canvas image constructor
+    const imageCanvas = new Image();
+    //give new canvas image attributes
+    imageCanvas.setAttribute('crossOrigin', 'anonymous');
+    //give new canvas image address
+    imageCanvas.src = img.src;
+    imageCanvas.onload = function() {
+        //the inner canvas size is equal to the image size
+        canvas.width = imageCanvas.width;
+        canvas.height = imageCanvas.height;
+        //drawing context
+        const ctx = canvas.getContext("2d");
+        //index to blur filter
+        let index = img.naturalHeight/img.height;
+        //use filtres to canvas image
+        ctx.filter = "blur(" + (blur * index) + "px)";
+        ctx.filter += "invert(" + invert + "%)";
+        ctx.filter += "sepia(" + sepia + "%)";
+        ctx.filter += "saturate(" + saturate + "%)";
+        ctx.filter += "hue-rotate(" + hue + "deg)";
+        //displayed canvas image
+        ctx.drawImage(imageCanvas, 0, 0);
+        //create link
+        let a = document.createElement("a");
+        //name of download canvas image
+        a.download = "image.png";
+        //create a temporary link href on which we will place 
+        //the canvas's data url, then click and delete
+        a.href = canvas.toDataURL();
+        a.click();
+        a.delete;
+    };
+});
